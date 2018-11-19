2fca1840775c0d29263dedc19c18deef1f725d73
Piotr Maślanka <piotr.maslanka@henrietta.com.pl>
2018-11-19 17:37:03 +0100

    code cleanup and version bump

--- setup.py
+++ setup.py
@@ -2,16 +2,17 @@
 from setuptools import setup, find_packages
 
 with open('README.md', 'r') as fin:
-      long_description = fin.read()
+    long_description = fin.read()
 
 setup(name='falcon-ratelimit',
-      version='1.1a1',
+      version='1.1a2',
       packages=find_packages(include=['falconratelimit', 'falconratelimit.*']),
       install_requires=["falcon", "six"],
       tests_require=[
-          "nose", "mock", "coverage"
+          "nose", "mock", "coverage", "freezegun"
       ],
       test_suite='nose.collector',
       long_description=long_description,
       description='A rate limiter plugin for Falcon'
       )
+
