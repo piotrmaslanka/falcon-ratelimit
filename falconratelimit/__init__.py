403231be95dfd232a7fbcb3e0ee89d1171de2b00
Piotr Maślanka <piotr.maslanka@henrietta.com.pl>
2018-11-19 20:13:31 +0100

    move version

--- falconratelimit/__init__.py
+++ falconratelimit/__init__.py
@@ -3,9 +3,7 @@ from __future__ import print_function, absolute_import, division
 
 from .ratelimit import rate_limit
 
-__version__ = '1.2a3'
 
 __all__ = [
     'rate_limit',
-    '__version__'
 ]
--- setup.py
+++ setup.py
@@ -5,10 +5,8 @@ with open('README.md', 'r') as fin:
     long_description = '\n'.join(
         x for x in fin.readlines() if not x.startswith('[!'))
 
-from falconratelimit import __version__
-
 setup(name='falcon-ratelimit',
-      version=__version__,
+      version='1.2a4',
       packages=find_packages(include=['falconratelimit', 'falconratelimit.*']),
       install_requires=["falcon", "six"],
       tests_require=[
