edd3a46c510c4b14897459734d933603c48f9021
Piotr Maślanka <piotr.maslanka@henrietta.com.pl>
2021-08-25 23:47:57 +0200

    v1.2

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md', 'r') as fin:
         x for x in fin.readlines() if not x.startswith('[!'))
 
 setup(name='falcon-ratelimit',
-      version='1.2a4',
+      version='1.2',
       packages=find_packages(include=['falconratelimit', 'falconratelimit.*']),
       install_requires=["falcon", "six"],
       tests_require=[
